9f3baf0c3b794b909175372822ef7522530c9a43
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2021-12-27 01:04:58 +0900

    🎉 Init

---
+++ .eslintrc.js
@@ -0,0 +1,107 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    project: "./tsconfig.json",
+    extraFileExtensions: [".mjs"],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    tailwindcss: { groupByResponsive: true },
+  },
+  plugins: ["simple-import-sort", "import", "jsx-a11y", "react-native"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:tailwindcss/recommended",
+    "prettier",
+  ],
+  rules: {
+    "no-console": [
+      "error",
+      {
+        allow: ["warn", "info", "error"],
+      },
+    ],
+    "no-restricted-syntax": [
+      "error",
+      {
+        selector: "TSEnumDeclaration",
+        message: "Don't declare enums",
+      },
+    ],
+    "prefer-arrow-callback": "error",
+    "prefer-const": "error",
+    "func-style": ["error", "expression"],
+    "no-restricted-imports": ["error", { paths: [{ name: "react", importNames: ["default"] }] }],
+    "react/display-name": "error",
+    "react/jsx-handler-names": [
+      "error",
+      {
+        eventHandlerPrefix: "handle",
+        eventHandlerPropPrefix: "on",
+        checkLocalVariables: true,
+        checkInlineFunction: true,
+      },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "import/newline-after-import": "error",
+    "import/no-default-export": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: ["typeAlias", "typeParameter"],
+        format: ["PascalCase"],
+      },
+      {
+        selector: ["property", "method"],
+        format: ["camelCase"],
+      },
+      {
+        selector: "variable",
+        types: ["boolean"],
+        format: ["PascalCase"],
+        prefix: ["no", "is", "has", "should"],
+        filter: {
+          regex: "^_",
+          match: false,
+        },
+      },
+    ],
+    "jsx-a11y/no-autofocus": "off",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["app/App.tsx"],
+      rules: { "import/no-default-export": "off" },
+    },
+  ],
+};
